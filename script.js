2b04ab2cd7a38e09f2ddfca956295e980e8607ef
TOS <horvath.sebestyen2013@gmail.com>
2026-07-07 19:02:36 +0200

    Implement localStorage for click counter
    Refactor click counter to use localStorage for persistence and update reset button visibility based on count.

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const gomb = document.querySelector('.main-gomb');
 const szöveg = document.querySelector('.szoveg');
 const resetGomb = document.querySelector('.reset-gomb');
 
-let kattintasSzamlalo = 0;
-resetGomb.style.display = 'none';
+let kattintasSzamlalo = Number(localStorage.getItem('libacombok')) || 0;
+if(kattintasSzamlalo === 0) {
+    resetGomb.style.display = 'none';
+} else {
+    resetGomb.style.display = 'block';
+    szöveg.textContent = "Kapsz " + kattintasSzamlalo + " libacombot!";
+}
 
 gomb.addEventListener('click', function() {
     kattintasSzamlalo++;
+    localStorage.setItem('libacombok', kattintasSzamlalo);
     szöveg.textContent = "Kapsz " + kattintasSzamlalo + " libacombot!";
     if(kattintasSzamlalo === 1) {
         resetGomb.style.display = 'block';
@@ -18,5 +24,6 @@ resetGomb.addEventListener('click', function() {
         kattintasSzamlalo = 0;
         szöveg.textContent = "";
         resetGomb.style.display = 'none';
+        localstorage.removeItem('libacombok');
     }
 });
