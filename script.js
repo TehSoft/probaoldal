0a4d7c8d72379f5ed721580a26c4c455b62a5772
TOS <horvath.sebestyen2013@gmail.com>
2026-07-07 18:28:14 +0200

    Rename button class and add reset functionality

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const gomb = document.querySelector('.sajat-gomb');
+const gomb = document.querySelector('.main-gomb');
 const szöveg = document.querySelector('.szoveg');
+const resetGomb = document.querySelector('.reset-gomb');
 
 let kattintasSzamlalo = 0;
+resetGomb.style.display = 'none';
 
 gomb.addEventListener('click', function() {
     kattintasSzamlalo++;
     szöveg.textContent = "Kapsz " + kattintasSzamlalo + " libacombot!";
+    if(kattintasSzamlalo ===1) {
+        resetGomb.style.display = 'block';
+    }
+});
+
+resetGomb.addEventListener('click', function() {
+    if(kattintasSzamlalo > 0) {
+        kattintasSzamlalo = 0;
+        szöveg.textContent = "";
+        resetGomb.style.display = 'none';
+    }
 });
